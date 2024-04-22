d86c24d4fa4149226aa067433f7895ec982a702b
ibdkn <ibudiakina@gmail.com>
2024-04-22 20:49:33 +0300

    added types in Message.tsx for props

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import s from './Message.module.css'
+import {MessageType} from "../HW1";
 
 // нужно создать правильный тип вместо any
-export type MessagePropsType = any
+export type MessagePropsType = {
+    message: MessageType
+}
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
+    console.log(props)
     return (
         <div id={'hw1-message-' + props.message.id} className={s.message}>
             <div className={s.imageAndText}>
