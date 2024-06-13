1ef3799867dbb04d21d2a1319fc6d3079229cb58
ibdkn <ibudiakina@gmail.com>
2024-06-13 23:04:03 +0300

    completed HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const sortedState = [...state];
 
-            return state // need to fix
+            if (action.payload === 'up') {
+                sortedState.sort((a, b) => a.name.localeCompare(b.name));
+            } else if (action.payload === 'down') {
+                sortedState.sort((a, b) => b.name.localeCompare(a.name));
+            }
+
+            return sortedState;
         }
         case 'check': {
-
-            return state // need to fix
+            const sortedState = [...state];
+            return sortedState.filter(user => user.age >= 18);
         }
         default:
             return state
